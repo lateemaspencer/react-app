eed13ef033c2f8b3d09e3b4cf9fe041b1c421a94
lateemaspencer <spencer.lateema@gmail.com>
2017-01-29 02:23:22 -0500

    Stateless function component child of parent component

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is state txt',
+    }
+  }
+
+  update( e ) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
+      </div>
+    )
+  }
+}
+
+const Widget = (props) =>
+  <input type='text' onChange={props.update}/>
+
+export default App
